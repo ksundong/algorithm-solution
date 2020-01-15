6406296c20fdf0cc3fadd4863051f67532c4acd6
IdionKim <ksundong@gmail.com>
2020-01-15 12:17:23 +0900

    Update sort number 3 using count sort
    메모리 사용 510816 KB -> 445844 KB
    시간 2280 ms -> 1504 ms
    배열의 크기는 숫자의 범위만큼 지정해주면된다.

--- src/main/java/dev/idion/baekjoon/sortnumber3/Main.java
+++ src/main/java/dev/idion/baekjoon/sortnumber3/Main.java
@@ -3,24 +3,24 @@ package dev.idion.baekjoon.sortnumber3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int numberCount = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
 
-        int[] numbers = new int[numberCount];
+        int numberCount = Integer.parseInt(bufferedReader.readLine());
+        int[] numbers = new int[10001];
         for (int i = 0; i < numberCount; i++) {
             int number = Integer.parseInt(bufferedReader.readLine());
-            numbers[i] = number;
+            numbers[number]++;
         }
 
-        Arrays.sort(numbers);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < numberCount; i++) {
-            stringBuilder.append(numbers[i]).append("\n");
+        for (int i = 0; i <= 10000; i++) {
+            while(numbers[i] != 0) {
+                stringBuilder.append(i).append("\n");
+                numbers[i]--;
+            }
         }
 
         System.out.println(stringBuilder.toString());
