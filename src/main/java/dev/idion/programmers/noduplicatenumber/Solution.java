7229adab2dd21d21b680dcc1f9b691b0bc5788b7
IdionKim <ksundong@gmail.com>
2020-02-05 19:40:56 +0900

    프로그래머스 같은 숫자는 싫어 시작

---
+++ src/main/java/dev/idion/programmers/noduplicatenumber/Solution.java
@@ -0,0 +1,8 @@
+package dev.idion.programmers.noduplicatenumber;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ src/test/java/dev/idion/programmers/noduplicatenumber/SolutionTest.java
@@ -0,0 +1,20 @@
+package dev.idion.programmers.noduplicatenumber;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SolutionTest {
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void testSolution1() {
+        fail();
+    }
+}
