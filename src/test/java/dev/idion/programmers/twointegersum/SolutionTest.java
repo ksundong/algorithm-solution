a577255593b69c7cd8a867f19fbd436aca832108
IdionKim <ksundong@gmail.com>
2020-02-06 17:36:41 +0900

    프로그래머스 두 정수 사이의 합 테스트 코드 추가

--- src/test/java/dev/idion/programmers/twointegersum/SolutionTest.java
+++ src/test/java/dev/idion/programmers/twointegersum/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.twointegersum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
     private Solution solution;
@@ -15,6 +15,28 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail();
+        int a = 3;
+        int b = 5;
+        long expected = 12;
+        long actual = solution.solution(a, b);
+        assertEquals(expected, actual, String.format("결과값 %d가 예상과 다릅니다.", actual));
+    }
+
+    @Test
+    void testSolution2() {
+        int a = 3;
+        int b = 3;
+        long expected = 3;
+        long actual = solution.solution(a, b);
+        assertEquals(expected, actual, String.format("결과값 %d가 예상과 다릅니다.", actual));
+    }
+
+    @Test
+    void testSolution3() {
+        int a = 5;
+        int b = 3;
+        long expected = 12;
+        long actual = solution.solution(a, b);
+        assertEquals(expected, actual, String.format("결과값 %d가 예상과 다릅니다.", actual));
     }
 }
