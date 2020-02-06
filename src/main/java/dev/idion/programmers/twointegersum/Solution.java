812e541480de28312a7b55427065738241db2361
IdionKim <ksundong@gmail.com>
2020-02-06 17:33:47 +0900

    프로그래머스 두 정수 사이의 합 시작

---
+++ src/main/java/dev/idion/programmers/twointegersum/Solution.java
@@ -0,0 +1,8 @@
+package dev.idion.programmers.twointegersum;
+
+public class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/dev/idion/programmers/twointegersum/SolutionTest.java
@@ -0,0 +1,20 @@
+package dev.idion.programmers.twointegersum;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SolutionTest {
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void testSolution1() {
+        fail();
+    }
+}
