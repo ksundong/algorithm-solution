ea09f54ac43fed55c75bc77330759f16e81f19f8
IdionKim <ksundong@gmail.com>
2020-02-09 22:51:50 +0900

    프로그래머스 문자열 다루기 기본 테스트 케이스 추가

--- src/test/java/dev/idion/programmers/basicstringcontrol/SolutionTest.java
+++ src/test/java/dev/idion/programmers/basicstringcontrol/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.basicstringcontrol;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
     Solution solution;
@@ -14,7 +14,18 @@ class SolutionTest {
     }
 
     @Test
-    void testSolution() {
-        fail();
+    void testSolution1() {
+        String s = "a234";
+        boolean expected = false;
+        boolean actual = solution.solution(s);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSolution2() {
+        String s = "1234";
+        boolean expected = true;
+        boolean actual = solution.solution(s);
+        assertEquals(expected, actual);
     }
 }
