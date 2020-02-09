7ad335fc5d224630fbeafcb208eb7ddc27fc8ca4
IdionKim <ksundong@gmail.com>
2020-02-09 22:50:04 +0900

    프로그래머스 문자열 다루기 기본 시작

---
+++ src/main/java/dev/idion/programmers/basicstringcontrol/Solution.java
@@ -0,0 +1,8 @@
+package dev.idion.programmers.basicstringcontrol;
+
+public class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        return answer;
+    }
+}
