84d0872709aae42e25f02ece125016f39c5c7bff
IdionKim <ksundong@gmail.com>
2020-02-11 23:40:35 +0900

    프로그래머스 소수 찾기 AssertJ를 활용한 테스트 코드 추가
    이후 부터는 AssertJ를 사용해서 테스트 코드 작성예정

--- src/test/java/dev/idion/programmers/primenumber/SolutionTest.java
+++ src/test/java/dev/idion/programmers/primenumber/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.primenumber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -14,7 +14,18 @@ class SolutionTest {
     }
 
     @Test
-    void testSolution() {
-        fail();
+    void testSolution1() {
+        int n = 10;
+        int expected = 4;
+        int actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int n = 5;
+        int expected = 3;
+        int actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
     }
 }
