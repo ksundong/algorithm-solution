85ed792dca2950590032aae90d839d48de900ba5
IdionKim <ksundong@gmail.com>
2020-02-13 00:02:58 +0900

    프로그래머스 수박수박수박수박수박수? 테스트 코드 작성

--- src/test/java/dev/idion/programmers/watermelon/SolutionTest.java
+++ src/test/java/dev/idion/programmers/watermelon/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.watermelon;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -14,7 +14,18 @@ class SolutionTest {
     }
 
     @Test
-    void solution() {
-        fail();
+    void testSolution1() {
+        int n = 3;
+        String expected = "수박수";
+        String actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int n = 4;
+        String expected = "수박수박";
+        String actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
     }
 }
