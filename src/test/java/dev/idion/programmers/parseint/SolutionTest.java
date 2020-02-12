a1a08785c9e4739a34e64ffe8a8fbabf70825fd7
IdionKim <ksundong@gmail.com>
2020-02-13 00:27:40 +0900

    프로그래머스 문자열을 정수로 바꾸기 테스트 코드 추가

--- src/test/java/dev/idion/programmers/parseint/SolutionTest.java
+++ src/test/java/dev/idion/programmers/parseint/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.parseint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -14,7 +14,18 @@ class SolutionTest {
     }
 
     @Test
-    void solution() {
-        fail();
+    void testSolution1() {
+        String s = "1234";
+        int expected = 1234;
+        int actual = solution.solution(s);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        String s = "-1234";
+        int expected = -1234;
+        int actual = solution.solution(s);
+        assertThat(actual).isEqualTo(expected);
     }
 }
