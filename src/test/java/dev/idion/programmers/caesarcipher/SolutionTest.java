a9ab7b9003205a577ece08c9850532457df00f4f
IdionKim <ksundong@gmail.com>
2020-02-13 21:51:11 +0900

    프로그래머스 시저 암호 테스트 케이스 추가

--- src/test/java/dev/idion/programmers/caesarcipher/SolutionTest.java
+++ src/test/java/dev/idion/programmers/caesarcipher/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.caesarcipher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -14,7 +14,29 @@ class SolutionTest {
     }
 
     @Test
-    void solution() {
-        fail("실패");
+    void testCase1() {
+        String s = "AB";
+        int n = 1;
+        String expected = "BC";
+        String actual = solution.solution(s, n);
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    void testCase2() {
+        String s = "z";
+        int n = 1;
+        String expected = "a";
+        String actual = solution.solution(s, n);
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    void testCase3() {
+        String s = "a B z";
+        int n = 4;
+        String expected = "e F d";
+        String actual = solution.solution(s, n);
+        assertThat(expected).isEqualTo(actual);
     }
 }
