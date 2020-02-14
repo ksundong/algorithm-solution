d386b03d3bc641009056b73a2fb6df44e8aea00f
IdionKim <ksundong@gmail.com>
2020-02-14 19:48:43 +0900

    프로그래머스 약수의 합 테스트 코드 추가

--- src/test/java/dev/idion/programmers/sumofmeasure/SolutionTest.java
+++ src/test/java/dev/idion/programmers/sumofmeasure/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.sumofmeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -15,6 +15,17 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        int n = 12;
+        int expected = 28;
+        int actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int n = 5;
+        int expected = 6;
+        int actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
     }
 }
