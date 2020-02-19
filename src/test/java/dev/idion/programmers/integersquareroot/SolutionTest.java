6284db4c9e3862d66e180e0f279942ec9c46fcc9
IdionKim <ksundong@gmail.com>
2020-02-19 20:28:59 +0900

    프로그래머스 정수 제곱근 판별 테스트 케이스 추가

--- src/test/java/dev/idion/programmers/integersquareroot/SolutionTest.java
+++ src/test/java/dev/idion/programmers/integersquareroot/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.integersquareroot;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -15,6 +15,18 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        long n = 121L;
+        long expected = 144;
+        long actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    void testSolution2() {
+        long n = 3L;
+        long expected = -1;
+        long actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
 }
