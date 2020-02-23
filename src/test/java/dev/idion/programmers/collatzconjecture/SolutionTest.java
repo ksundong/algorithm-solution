8da8ba8163211a4e7ac45e7db43aada903a730eb
IdionKim <ksundong@gmail.com>
2020-02-23 21:58:33 +0900

    프로그래머스 콜라츠 추측 테스트 케이스 추가

--- src/test/java/dev/idion/programmers/collatzconjecture/SolutionTest.java
+++ src/test/java/dev/idion/programmers/collatzconjecture/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.collatzconjecture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -15,6 +15,25 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        int n = 6;
+        int expected = 8;
+        int actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int n = 16;
+        int expected = 4;
+        int actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution3() {
+        int n = 626331;
+        int expected = -1;
+        int actual = solution.solution(n);
+        assertThat(actual).isEqualTo(expected);
     }
 }
