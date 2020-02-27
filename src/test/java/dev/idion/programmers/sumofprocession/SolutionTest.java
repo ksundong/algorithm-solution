6b5a4c95ae6eaad47fc211ef03fc2b8b69fa6cd5
IdionKim <ksundong@gmail.com>
2020-02-27 10:18:08 +0900

    프로그래머스 행렬의 덧셈 테스트 케이스 추가

--- src/test/java/dev/idion/programmers/sumofprocession/SolutionTest.java
+++ src/test/java/dev/idion/programmers/sumofprocession/SolutionTest.java
@@ -1,10 +1,9 @@
 package dev.idion.programmers.sumofprocession;
 
-import dev.idion.programmers.maskingphonenumber.Solution;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -16,6 +15,19 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        int[][] arr1 = {{1, 2}, {2, 3}};
+        int[][] arr2 = {{3, 4}, {5, 6}};
+        int[][] expected = {{4, 6}, {7, 9}};
+        int[][] actual = solution.solution(arr1, arr2);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int[][] arr1 = {{1}, {2}};
+        int[][] arr2 = {{3}, {4}};
+        int[][] expected = {{4}, {6}};
+        int[][] actual = solution.solution(arr1, arr2);
+        assertThat(actual).isEqualTo(expected);
     }
 }
