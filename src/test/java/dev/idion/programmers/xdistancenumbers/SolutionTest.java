f160f582a07132c1b4e1ca068918ea5708c6ce1f
IdionKim <ksundong@gmail.com>
2020-02-28 12:59:49 +0900

    프로그래머스 x만큼 간격이 있는 n개의 숫자 테스트케이스 추가

--- src/test/java/dev/idion/programmers/xdistancenumbers/SolutionTest.java
+++ src/test/java/dev/idion/programmers/xdistancenumbers/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.xdistancenumbers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -15,6 +15,28 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        int x = 2;
+        int n = 5;
+        long[] expected = {2L, 4L, 6L, 8L, 10L};
+        long[] actual = solution.solution(x, n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int x = 4;
+        int n = 3;
+        long[] expected = {4L, 8L, 12L};
+        long[] actual = solution.solution(x, n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution3() {
+        int x = -4;
+        int n = 2;
+        long[] expected = {-4L, -8L};
+        long[] actual = solution.solution(x, n);
+        assertThat(actual).isEqualTo(expected);
     }
 }
