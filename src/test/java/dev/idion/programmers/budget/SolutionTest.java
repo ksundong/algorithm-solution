23f0ea8d52a509fbb7381308cff08f37110724aa
IdionKim <ksundong@gmail.com>
2020-03-01 16:37:22 +0900

    프로그래머스 예산 테스트 케이스 추가

--- src/test/java/dev/idion/programmers/budget/SolutionTest.java
+++ src/test/java/dev/idion/programmers/budget/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.budget;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -15,6 +15,19 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        int[] d = {1, 3, 2, 5, 4};
+        int budget = 9;
+        int expected = 3;
+        int actual = solution.solution(d, budget);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int[] d = {2, 2, 3, 3};
+        int budget = 10;
+        int expected = 4;
+        int actual = solution.solution(d, budget);
+        assertThat(actual).isEqualTo(expected);
     }
 }
