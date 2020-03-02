a2019ab43d36ee2828c14015e98a15942466f18f
IdionKim <ksundong@gmail.com>
2020-03-02 09:10:28 +0900

    프로그래머스 비밀지도 테스트케이스 추가

--- src/test/java/dev/idion/programmers/secretmap/SolutionTest.java
+++ src/test/java/dev/idion/programmers/secretmap/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.secretmap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -15,6 +15,21 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+        String[] expected = {"#####", "# # #", "### #", "# ##", "#####"};
+        String[] actual = solution.solution(n, arr1, arr2);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int n = 6;
+        int[] arr1 = {46, 33, 33, 22, 31, 50};
+        int[] arr2 = {27, 56, 19, 14, 14, 10};
+        String[] expected = {"######", "### #", "## ##", " #### ", " #####", "### # "};
+        String[] actual = solution.solution(n, arr1, arr2);
+        assertThat(actual).isEqualTo(expected);
     }
 }
