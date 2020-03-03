6bd99d18c9b140b621b86ef122fe0f5b4e73e824
IdionKim <ksundong@gmail.com>
2020-03-03 15:11:12 +0900

    프로그래머스 실패율 테스트 케이스 추가

--- src/test/java/dev/idion/programmers/failurerate/SolutionTest.java
+++ src/test/java/dev/idion/programmers/failurerate/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.failurerate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -15,6 +15,19 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        int n = 5;
+        int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+        int[] expected = {3, 4, 2, 1, 5};
+        int[] actual = solution.solution(n, stages);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        int n = 4;
+        int[] stages = {4, 4, 4, 4, 4};
+        int[] expected = {4, 1, 2, 3};
+        int[] actual = solution.solution(n, stages);
+        assertThat(actual).isEqualTo(expected);
     }
 }
