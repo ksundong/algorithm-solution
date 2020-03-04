4ba1b388c28c6f1e2ba91b595da6e81f4be3a477
IdionKim <ksundong@gmail.com>
2020-03-04 17:01:40 +0900

    프로그래머스 다트 게임 테스트 케이스 추가

--- src/test/java/dev/idion/programmers/dartgame/SolutionTest.java
+++ src/test/java/dev/idion/programmers/dartgame/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.programmers.dartgame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -15,6 +15,57 @@ class SolutionTest {
 
     @Test
     void testSolution1() {
-        fail("실패");
+        String dartResult = "1S2D*3T";
+        int expected = 37;
+        int actual = solution.solution(dartResult);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution2() {
+        String dartResult = "1D2S#10S";
+        int expected = 9;
+        int actual = solution.solution(dartResult);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution3() {
+        String dartResult = "1D2S0T";
+        int expected = 3;
+        int actual = solution.solution(dartResult);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution4() {
+        String dartResult = "1S*2T*3S";
+        int expected = 23;
+        int actual = solution.solution(dartResult);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution5() {
+        String dartResult = "1D#2S*3S";
+        int expected = 5;
+        int actual = solution.solution(dartResult);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution6() {
+        String dartResult = "1T2D3D#";
+        int expected = -4;
+        int actual = solution.solution(dartResult);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSolution7() {
+        String dartResult = "1D2S3T*";
+        int expected = 59;
+        int actual = solution.solution(dartResult);
+        assertThat(actual).isEqualTo(expected);
     }
 }
