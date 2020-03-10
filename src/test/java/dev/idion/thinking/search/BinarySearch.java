7a873adb59faaf09f924d5aa53649e798918b277
IdionKim <ksundong@gmail.com>
2020-03-10 10:54:43 +0900

    이진 탐색 테스트 코드 작성

---
+++ src/test/java/dev/idion/thinking/search/BinarySearch.java
@@ -0,0 +1,7 @@
+package dev.idion.thinking.search;
+
+public class BinarySearch {
+    public int searchRecursive(int[] arr, int target, int firstIndex, int lastIndex) {
+        return 0;
+    }
+}
---
+++ src/test/java/dev/idion/thinking/search/BinarySearchTest.java
@@ -0,0 +1,37 @@
+package dev.idion.thinking.search;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BinarySearchTest {
+    BinarySearch binarySearch;
+
+    @BeforeEach
+    void setUp() {
+        binarySearch = new BinarySearch();
+    }
+
+    @Test
+    void testSearchRecursive1() {
+        int[] arr = {1, 3, 5, 7, 8, 13, 23, 34};
+        int target = 23;
+        int firstIndex = 0;
+        int lastIndex = arr.length - 1;
+        int expected = 6;
+        int actual = binarySearch.searchRecursive(arr, target, firstIndex, lastIndex);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testSearchRecursive2() {
+        int[] arr = {1, 3, 5, 7, 8, 13, 23, 34};
+        int target = 43;
+        int firstIndex = 0;
+        int lastIndex = arr.length - 1;
+        int expected = -1;
+        int actual = binarySearch.searchRecursive(arr, target, firstIndex, lastIndex);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
