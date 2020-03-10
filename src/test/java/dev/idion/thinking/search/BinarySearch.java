fbf0ddd48983ec5b63c337601930ccc3ad0b5c96
IdionKim <ksundong@gmail.com>
2020-03-10 10:57:37 +0900

    이진 탐색 코드 작성

--- src/test/java/dev/idion/thinking/search/BinarySearch.java
+++ src/test/java/dev/idion/thinking/search/BinarySearch.java
@@ -2,6 +2,19 @@ package dev.idion.thinking.search;
 
 public class BinarySearch {
     public int searchRecursive(int[] arr, int target, int firstIndex, int lastIndex) {
-        return 0;
+        if (firstIndex >= lastIndex) {
+            return -1;
+        }
+
+        int middleIndex = (firstIndex + lastIndex) / 2;
+        int middleValue = arr[middleIndex];
+
+        if (target == middleValue) {
+            return middleIndex;
+        } else if (target < middleValue) {
+            return searchRecursive(arr, target, firstIndex, middleIndex - 1);
+        } else {
+            return searchRecursive(arr, target, middleIndex + 1, lastIndex);
+        }
     }
 }
