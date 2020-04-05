4ab201ada42e3717ff49b228aa1728df58c2a097
IdionKim <ksundong@gmail.com>
2020-04-05 21:40:59 +0900

    LeetCode Palindrome Number 테스트 코드 작성

--- src/test/java/dev/idion/leetcode/palindromenumber/SolutionTest.java
+++ src/test/java/dev/idion/leetcode/palindromenumber/SolutionTest.java
@@ -3,7 +3,7 @@ package dev.idion.leetcode.palindromenumber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     Solution solution;
@@ -14,7 +14,26 @@ class SolutionTest {
     }
 
     @Test
-    void testIsPalindrome() {
-        fail("실패");
+    void testIsPalindrome1() {
+        int x = 121;
+        boolean expected = true;
+        boolean actual = solution.isPalindrome(x);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testIsPalindrome2() {
+        int x = -121;
+        boolean expected = false;
+        boolean actual = solution.isPalindrome(x);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testIsPalindrome3() {
+        int x = 10;
+        boolean expected = false;
+        boolean actual = solution.isPalindrome(x);
+        assertThat(actual).isEqualTo(expected);
     }
 }
