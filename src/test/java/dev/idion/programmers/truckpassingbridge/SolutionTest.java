eb9d8f544256a489a88114ea941a82a8add4481d
IdionKim <ksundong@gmail.com>
2020-06-07 20:09:54 +0900

    프로그래머스 다리를 지나는 트럭 실패하는 테스트 생성

---
+++ src/test/java/dev/idion/programmers/truckpassingbridge/SolutionTest.java
@@ -0,0 +1,22 @@
+package dev.idion.programmers.truckpassingbridge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+class SolutionTest {
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    @DisplayName("test1")
+    void test1() {
+        fail("Not Implemented");
+    }
+}
