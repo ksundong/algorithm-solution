6d45a45066c9450bd0ac4449a96c7a4a965d899f
IdionKim <ksundong@gmail.com>
2020-06-07 20:13:28 +0900

    프로그래머스 다리를 지나는 트럭 테스트케이스 추가

--- src/test/java/dev/idion/programmers/truckpassingbridge/SolutionTest.java
+++ src/test/java/dev/idion/programmers/truckpassingbridge/SolutionTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
     private Solution solution;
@@ -17,6 +17,39 @@ class SolutionTest {
     @Test
     @DisplayName("test1")
     void test1() {
-        fail("Not Implemented");
+        int bridgeLength = 2;
+        int weight = 10;
+        int[] truckWeights = {7, 4, 5, 6};
+        int expected = 8;
+
+        int actual = solution.solution(bridgeLength, weight, truckWeights);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("test2")
+    void test2() {
+        int bridgeLength = 100;
+        int weight = 100;
+        int[] truckWeights = {10};
+        int expected = 101;
+
+        int actual = solution.solution(bridgeLength, weight, truckWeights);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("test3")
+    void test3() {
+        int bridgeLength = 100;
+        int weight = 100;
+        int[] truckWeights = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        int expected = 110;
+
+        int actual = solution.solution(bridgeLength, weight, truckWeights);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
