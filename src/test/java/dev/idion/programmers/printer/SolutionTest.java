d53227f53ab3155a2b5470259a99942c10638311
IdionKim <ksundong@gmail.com>
2020-07-04 19:23:16 +0900

    프로그래머스 프린터 테스트 코드 추가

--- src/test/java/dev/idion/programmers/printer/SolutionTest.java
+++ src/test/java/dev/idion/programmers/printer/SolutionTest.java
@@ -1,6 +1,6 @@
 package dev.idion.programmers.printer;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,8 +16,20 @@ class SolutionTest {
   }
 
   @Test
-  @DisplayName("예제 테스트")
+  @DisplayName("예제 테스트1")
   void test1() {
-    fail("Not Implemented");
+    int[] priorities = {2, 1, 3, 2};
+    int location = 2;
+    int expected = 1;
+    assertThat(solution.solution(priorities, location)).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("예제 테스트2")
+  void test2() {
+    int[] priorities = {1, 1, 9, 1, 1, 1};
+    int location = 0;
+    int expected = 5;
+    assertThat(solution.solution(priorities, location)).isEqualTo(expected);
   }
 }
