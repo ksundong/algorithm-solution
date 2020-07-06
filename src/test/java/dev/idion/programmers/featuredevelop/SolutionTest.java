a2c41ed0a615c52d0e0215fcb46b51b7ce8a24d4
IdionKim <ksundong@gmail.com>
2020-07-06 09:03:54 +0900

    프로그래머스 기능개발 예제 테스트 코드 추가

--- src/test/java/dev/idion/programmers/featuredevelop/SolutionTest.java
+++ src/test/java/dev/idion/programmers/featuredevelop/SolutionTest.java
@@ -1,15 +1,26 @@
 package dev.idion.programmers.featuredevelop;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
+  Solution solution;
+
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
   @Test
   @DisplayName("Solution Test")
   void solutionTest() {
-    fail("Not Implemented");
+    int[] progresses = {93, 30, 55};
+    int[] speeds = {1, 30, 5};
+    int[] expected = {2, 1};
+    assertThat(solution.solution(progresses, speeds)).isEqualTo(expected);
   }
 }
