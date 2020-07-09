dea4ff27383627b93aadc6ffb5ed0c117164c4e5
IdionKim <ksundong@gmail.com>
2020-07-09 11:27:57 +0900

    프로그래머스 멀쩡한 사각형 풀이 완료

--- src/main/java/dev/idion/programmers/finesquare/Solution.java
+++ src/main/java/dev/idion/programmers/finesquare/Solution.java
@@ -2,7 +2,12 @@ package dev.idion.programmers.finesquare;
 
 public class Solution {
   public long solution(int w, int h) {
-    long answer = 1;
-    return answer;
+    int gcf = 0;
+    for (int i = 1; i <= Math.max(w, h); i++) {
+      if (w % i == 0 && h % i == 0) {
+        gcf = i;
+      }
+    }
+    return (long) w * h - (w + h - gcf);
   }
 }
