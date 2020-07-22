65e41cc223a1024c7b7decee5dc6fef7df467471
IdionKim <ksundong@gmail.com>
2020-07-22 20:00:18 +0900

    백준 디지털 티비 풀이 완료

--- src/main/java/dev/idion/baekjoon/digitaltv/Main.java
+++ src/main/java/dev/idion/baekjoon/digitaltv/Main.java
@@ -8,7 +8,43 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int size = Integer.parseInt(br.readLine());
+    String[] kbs = {"KBS1", "KBS2"};
+    StringBuilder sb = new StringBuilder();
 
+    int k1 = -1;
+    int k2 = -1;
+
+    for (int i = 0; i < size; i++) {
+      String s = br.readLine();
+      if (s.equals(kbs[0])) {
+        k1 = i;
+      } else if (s.equals(kbs[1])) {
+        k2 = i;
+      }
+      if (k1 != -1 && k2 != -1) {
+        break;
+      }
+    }
+
+    int inOrder = k1 > k2 ? 1 : 0; // k1이 더 뒤에 있으면 k2가 뒤로 이동하므로 한 번 더 움직임.
+    for (int i = 0; i < k1; i++) {
+      sb.append(1);
+    }
+
+    for (int i = 0; i < k1; i++) {
+      sb.append(4);
+    }
+
+    for (int i = 0; i < k2 + inOrder; i++) {
+      sb.append(1);
+    }
+
+    for (int i = 0; i < k2 + inOrder - 1; i++) {
+      sb.append(4);
+    }
+
+    System.out.print(sb.toString());
     br.close();
   }
 }
--- src/test/java/dev/idion/baekjoon/digitaltv/MainTest.java
+++ src/test/java/dev/idion/baekjoon/digitaltv/MainTest.java
@@ -1,9 +1,7 @@
 package dev.idion.baekjoon.digitaltv;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import dev.idion.baekjoon.findprime.Main;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -14,12 +12,37 @@ import org.junit.jupiter.api.Test;
 
 class MainTest {
 
-
   @Test
   @DisplayName("solution test1")
   void solutionTest1() throws IOException {
     String input = "3\nMBC\nKBS1\nKBS2";
-    String expected = "33";
+    String expected = "14114";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(out));
+    System.setIn(in);
+    Main.main(null);
+    assertThat(out.toString()).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("solution test2")
+  void solutionTest2() throws IOException {
+    String input = "5\nMBC\nSBS\nJTBC\nKBS1\nKBS2";
+    String expected = "1114441111444";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(out));
+    System.setIn(in);
+    Main.main(null);
+    assertThat(out.toString()).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("solution test3")
+  void solutionTest3() throws IOException {
+    String input = "5\nMBC\nSBS\nJTBC\nKBS2\nKBS1";
+    String expected = "111144441111444";
     InputStream in = new ByteArrayInputStream(input.getBytes());
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     System.setOut(new PrintStream(out));
