26682e260a00e487170383d25f3b35438be69074
IdionKim <ksundong@gmail.com>
2020-07-23 15:53:21 +0900

    백준 30 풀이 개선

--- src/main/java/dev/idion/baekjoon/thirty/Main.java
+++ src/main/java/dev/idion/baekjoon/thirty/Main.java
@@ -3,40 +3,32 @@ package dev.idion.baekjoon.thirty;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String s = br.readLine();
-    int length = s.length();
-    int[] nums = new int[length];
-    String[] strs = s.split("");
-
-    for (int i = 0; i < length; i++) {
-      nums[i] = Integer.parseInt(strs[i]);
-    }
-
-    Arrays.sort(nums);
+    int[] nums = new int[10];
 
     int sum = 0;
-    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      int num = s.charAt(i) - '0';
+      sum += num;
+      nums[num]++;
+    }
 
-    if (nums[0] != 0) {
+    StringBuilder sb = new StringBuilder();
+    if (nums[0] == 0 || sum % 3 != 0) {
       sb = new StringBuilder("-1");
     } else {
-      for (int i = length - 1; i >= 0; i--) {
-        int num = nums[i];
-        sum += num;
-        sb.append(num);
-      }
-
-      if (sum % 3 != 0) {
-        sb = new StringBuilder("-1");
+      for (int i = 9; i >= 0; i--) {
+        for (int j = 0; j < nums[i]; j++) {
+          sb.append(i);
+        }
       }
     }
 
-    System.out.print(sb.toString());
+    System.out.print(sb);
   }
 }
