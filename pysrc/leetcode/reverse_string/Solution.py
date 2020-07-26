bb969aa4a5718446dc495c2a24f686c4cb1f7857
IdionKim <ksundong@gmail.com>
2020-07-26 18:55:30 +0900

    문자열 뒤집기 파이썬 다운 방식으로 풀기2

--- pysrc/leetcode/reverse_string/Solution.py
+++ pysrc/leetcode/reverse_string/Solution.py
@@ -3,5 +3,5 @@ from typing import List
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        # Pythonic way 1 reverse 함수 활용하기
-        s.reverse()
+        # Pythonic way 2 슬라이싱 활용하기(문자열에도 사용가능)
+        s[:] = s[::-1]  # s = s[::-1] 은 동작안해서 사용하는 트릭
