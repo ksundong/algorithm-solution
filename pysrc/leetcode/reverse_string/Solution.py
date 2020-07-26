8ddb17a28ba0be3781251a75c6f7f7b65fc1b7bc
IdionKim <ksundong@gmail.com>
2020-07-26 18:55:19 +0900

    문자열 뒤집기 파이썬 다운 방식으로 풀기

--- pysrc/leetcode/reverse_string/Solution.py
+++ pysrc/leetcode/reverse_string/Solution.py
@@ -3,10 +3,5 @@ from typing import List
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        # 왼쪽과 오른쪽 포인터 지정
-        left, right = 0, len(s) - 1
-        while left < right:
-            # 이 부분을 보고 되게 신기했는데, 별도의 tmp 변수가 없어도 된다.
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
+        # Pythonic way 1 reverse 함수 활용하기
+        s.reverse()
