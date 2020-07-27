89797840c0d1cabc62b2a6a81cdc808f7955a6b8
IdionKim <ksundong@gmail.com>
2020-07-27 17:57:41 +0900

    주식을 사고팔기 가장 좋은 시점 브루트포스 풀이 개선 완료

--- pysrc/leetcode/best_time_to_buy_and_sell_stock/Solution.py
+++ pysrc/leetcode/best_time_to_buy_and_sell_stock/Solution.py
@@ -1,12 +1,16 @@
+import sys
 from typing import List
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit: int = 0
-        for i in range(len(prices) - 1):
-            max_profit = max(max_profit, max(prices[i + 1:]) - prices[i])
+        max_profit: int = 0  # 최소 0이다.
+        min_price: int = sys.maxsize  # 항상 갱신될 수 있도록 한다.
+        for price in prices:  # 최대 이익과 최소 가격을 계속 갱신한다.
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
         return max_profit
+
 
 print(Solution.maxProfit(None, [7, 1, 5, 3, 6, 4]))
 print(Solution.maxProfit(None, [7, 6, 4, 3, 1]))
