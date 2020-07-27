03c90545ef581883df8a0f05ebad5aed6cdefc1c
IdionKim <ksundong@gmail.com>
2020-07-27 16:14:35 +0900

    Pythonic하게 풀기

--- pysrc/leetcode/array-partition-i/Solution.py
+++ pysrc/leetcode/array-partition-i/Solution.py
@@ -3,11 +3,7 @@ from typing import List
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        sum_of_nums: int = 0
-        for num in nums[::2]:
-            sum_of_nums += num
-        return sum_of_nums
+        return sum(sorted(nums)[::2])
 
 
 print(Solution.arrayPairSum(None, [1, 4, 3, 2]))
