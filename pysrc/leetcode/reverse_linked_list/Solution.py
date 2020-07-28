9cf2856ed23dd0fd8e20b6d9649e4a6717bafd78
IdionKim <ksundong@gmail.com>
2020-07-28 17:33:36 +0900

    역순 연결 리스트 재귀로 풀이

--- pysrc/leetcode/reverse_linked_list/Solution.py
+++ pysrc/leetcode/reverse_linked_list/Solution.py
@@ -7,13 +7,13 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        dummy = ListNode()
+        def reverse(node: ListNode, prev: ListNode = None):
+            if not node:  # 종료조건, 노드가 존재하지 않는다면 끝낸다.
+                return prev
+            next_node, node.next = node.next, prev  # 다음 노드 참조에 현재 노드의 다음 노드 참조를 넣고, 다음 노드 참조에 이전 노드를 넣는다.
+            return reverse(next_node, node)  # 재귀 구간(다음 노드에, 이전노드인 현재 노드를 넣는 과정)
 
-        node = head
-        while node:  # 노드를 순회하면서 더미 리스트의 헤드 부분에 붙인다.
-            dummy.next, node.next, node = node, dummy.next, node.next
-
-        return dummy.next
+        return reverse(head)
 
 
 print(Solution.reverseList(None, ListNode(1, ListNode(2, ListNode(3)))))
