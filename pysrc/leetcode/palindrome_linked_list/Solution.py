e86bb08f67793e3e5f08a6a20e6caf2dfa4593c6
IdionKim <ksundong@gmail.com>
2020-07-28 15:23:26 +0900

    팰린드롬 연결리스트 풀이 공부

--- pysrc/leetcode/palindrome_linked_list/Solution.py
+++ pysrc/leetcode/palindrome_linked_list/Solution.py
@@ -1,8 +1,4 @@
 # Definition for singly-linked list.
-from collections import deque
-from typing import Deque
-
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -11,23 +7,19 @@ class ListNode:
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        # deque 선언 및 저장
-        nodes: Deque = deque()
+        rev = None
+        slow = fast = head
+        # 런너를 이용해 역순 연결리스트 구성
+        while fast and fast.next:  # 두 칸씩 건너뛰기 위해 확인
+            fast = fast.next.next  # 두 칸씩 건너뛴다.
+            rev, rev.next, slow = slow, rev, slow.next  # rev는 역순으로 노드를 저장한다.
+        if fast:  # 홀수개라면
+            slow = slow.next  # 가운데 노드는 건너간다.
 
-        if not head:  # head가 None이면...
-            return True
-
-        node = head
-        while node is not None:
-            nodes.append(node.val)
-            node = node.next
-
-        # deque의 앞 뒤를 비교해서 다르면 팰린드롬이 아님.
-        while len(nodes) > 1:
-            if nodes.pop() != nodes.popleft():
-                return False
-
-        return True
+        # 팰린드롬 여부 확인
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev  # rev가 존재하면 값이 다른경우다.
 
 
 print(Solution.isPalindrome(None, ListNode(1, ListNode(2))))
