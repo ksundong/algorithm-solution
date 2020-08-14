69c39588a6ff04e625ff07d2f5158d1e5f99e86d
IdionKim <ksundong@gmail.com>
2020-08-14 22:43:51 +0900

    스택을 이용한 큐 구현 풀이

--- pysrc/leetcode/implement_queue_using_stacks/Solution.py
+++ pysrc/leetcode/implement_queue_using_stacks/Solution.py
@@ -1,29 +1,30 @@
 class MyQueue:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        self.stack = []
+        self.substack = []
 
     def push(self, x: int) -> None:
-        """
-        Push element x to the back of queue.
-        """
+        self.stack.append(x)
 
     def pop(self) -> int:
-        """
-        Removes the element from in front of queue and returns that element.
-        """
+        for _ in range(len(self.stack) - 1):
+            self.substack.append(self.stack.pop(-1))
+        ret_val = self.stack.pop(-1)
+        for _ in range(len(self.substack)):
+            self.stack.append(self.substack.pop(-1))
+        return ret_val
 
     def peek(self) -> int:
-        """
-        Get the front element.
-        """
+        for _ in range(len(self.stack) - 1):
+            self.substack.append(self.stack.pop(-1))
+        ret_val = self.stack[-1]
+        for _ in range(len(self.substack)):
+            self.stack.append(self.substack.pop(-1))
+        return ret_val
 
     def empty(self) -> bool:
-        """
-        Returns whether the queue is empty.
-        """
+        return len(self.stack) == 0
 
 
 # Your MyQueue object will be instantiated and called as such:
