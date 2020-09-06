1f09db9d8eef6be841d395152e65df54e85572b2
IdionKim <ksundong@gmail.com>
2020-09-07 08:13:14 +0900

    feat: 조합 풀이 완료(itertools 사용)

--- pysrc/leetcode/combinations/Solution.py
+++ pysrc/leetcode/combinations/Solution.py
@@ -1,2 +1,11 @@
+from itertools import combinations
+from typing import List
+
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(combinations(range(1, n + 1), k))
+
+
+solution = Solution()
+print(solution.combine(4, 2))
