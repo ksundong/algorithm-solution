fae36834fdab51978972dc7fce2212bd2391ccc5
IdionKim <ksundong@gmail.com>
2020-09-10 16:21:39 +0900

    refactor: 다른 사람 풀이 참고
    너무 깔끔한 코드라 베껴보았다.

--- pysrc/programmers/카카오 블라인드 채용(2018)/다트 게임/solution.py
+++ pysrc/programmers/카카오 블라인드 채용(2018)/다트 게임/solution.py
@@ -3,25 +3,16 @@ import re
 
 def solution(dartResult):
     s_dict = {'S': 1, 'D': 2, 'T': 3}
-    scores = [0] * 3
+    option = {'': 1, '*': 2, '#': -1}
 
-    match = re.finditer(r'(\d+[SDT][*#]?)', dartResult)
-    for j, m in enumerate(match):
-        match2 = re.match(r'(\d+)([SDT]+)([*#]?)', m.group())
-        for i, m2 in enumerate(match2.groups()):
-            if i == 0:  # 숫자
-                scores[j] = int(m2)
-            if i == 1:  # SDT
-                scores[j] **= s_dict[m2]
-            if i == 2:  # 옵션
-                if m2 == '*':
-                    if j > 0:
-                        scores[j - 1] *= 2
-                    scores[j] *= 2
-                if m2 == '#':
-                    scores[j] *= -1
+    pattern = re.compile(r'(\d+)([SDT])([*#]?)')
+    dart = pattern.findall(dartResult)
+    for i in range(len(dart)):
+        if i > 0 and dart[i][2] == '*':
+            dart[i - 1] *= 2
+        dart[i] = int(dart[i][0]) ** s_dict[dart[i][1]] * option[dart[i][2]]
 
-    return sum(scores)
+    return sum(dart)
 
 
 solution('1D2S#10S')
