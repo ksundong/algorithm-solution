e7c5d68a5f62874aa2548becfc096f64b05f7e90
IdionKim <ksundong@gmail.com>
2020-09-15 11:38:40 +0900

    feat: LeetCode 코스 스케줄 풀이 참조

--- pysrc/leetcode/course_schedule/Solution.py
+++ pysrc/leetcode/course_schedule/Solution.py
@@ -9,9 +9,24 @@ class Solution:
         for x, y in prerequisites:
             graph[x].append(y)
 
+        # 순환구조를 파악하기 위한 자료구조
+        traced = set()
+
+        def dfs(a):
+            if a in traced:  # 이미 방문한 경우 순환구조임
+                return False
+
+            traced.add(a)  # 방문한 위치 저장
+            for b in graph[a]:  # 리스트 형태로 관리됨
+                if not dfs(b):  # 탈출 조건(내부에서 실패시)
+                    return False
+            traced.remove(a)  # 방문 이력 제거
+
+            return True
+
+        # graph의 크기가 변경되어서 오류 발생해서 list로 변경해줌(?)
+        for x in list(graph):
+            if not dfs(x):  # dfs에서 실패하는 경우 순환구조
+                return False
+
         return True
-
-
-solution = Solution()
-solution.canFinish(2, [[1, 0]])  # True
-solution.canFinish(2, [[1, 0], [0, 1]])  # False
