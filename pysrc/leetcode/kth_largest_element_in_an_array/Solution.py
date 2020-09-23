819b0215f0aaf0e04d0a1e1e2f31c81f049d971d
IdionKim <ksundong@gmail.com>
2020-09-23 13:58:13 +0900

    refactor: 배열의 K번째 큰 요소 풀이 개선

--- pysrc/leetcode/kth_largest_element_in_an_array/Solution.py
+++ pysrc/leetcode/kth_largest_element_in_an_array/Solution.py
@@ -1,15 +1,6 @@
-import heapq
 from typing import List
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for num in nums:
-            heapq.heappush(heap, -num)
-
-        answer = 0
-        for i in range(k):
-            answer = -heapq.heappop(heap)
-
-        return answer
+        return sorted(nums)[-k]
